e039aaeb757281d8038e7de982e9051799fdca9d
24HP3934 24HP3934 <24HP3934@admins-iMac.local>
2024-11-15 18:39:29 +0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import * as fs from "fs";
 import { nanoid } from "nanoid";
 
-
 const PORT = 3333;
 const app = express();
 app.use(express.json());
@@ -51,19 +50,37 @@ app.delete("/todos/:id", (req, res) => {
 });
 
 app.put("/todos/:id", (req, res) => {
-  const id = req.params.id;
+  const title = req.params.title;
   const updatedData = req.body;
-  console.log(id, body);
+  console.log(updatedData);
   if (!id) return res.status(400).send({ message: "Id not found!" });
   const todoIndex = todos.find((item) => item.id === Number(id));
   if (todoIndex === -1) {
     return res.status(404).send({ message: "Todo not found!" });
   }
-  items[itemsIndex] = {
-    updatedData,
-  };
+
+  fs.writeFileSync("./data.json", JSON.stringify(todos), "utf-8");
   return res.send({ message: "Todo updated!!!" });
-  // TODO update functions
+});
+
+app.post("/Signup", (req, res) => {
+  const { name, email, password } = req.body;
+  const users = JSON.parse(fs.readFileSync("./users.json", "utf-8"));
+  const existingUser = users.find((user) => user.email === email);
+  if (existingUser)
+    return res.status(400).send({ message: "user already registered" });
+  const newUsers = {
+    id: nanoid(),
+    name: name,
+    email: email,
+    password: password,
+  };
+  fs.writeFileSync(
+    "./users.json",
+    JSON.stringify([...users, newUsers]),
+    "utf-8"
+  );
+  return res.status(200).json({ message: "New user created" });
 });
 
 app.listen(PORT, () => {
